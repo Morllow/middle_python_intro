9b985b747b4e95c4fd15ffa2f2e555486f7b831d
Morllow <morllow@yandex.ru>
2023-01-19 17:57:20 +0300

    Исправлена функция и стили

--- app/main.py
+++ app/main.py
@@ -1,14 +1,34 @@
 """Генератор приветствий."""
-import pprint
 
-def Greeting(name: str) -> str:
-      """Возвращает текст приветствия.
 
-      Args:
-          name: Имя пользователя
+def set_first_letters_to_uppercase(source_value: str) -> str:
+    """Возвращает строку.
 
-      Returns:
-          int: Текст приветствия
-      """
-      # pprint.pprint(name.lower())
-      return 'Привет, name'
+    Возвращает строку с первыми заглавнными
+    буквами в отдельных словах.
+
+    Args:
+        source_value: Исходная строка
+
+    Returns:
+        str: строка с первыми заглавными буквами
+    """
+    source_value = source_value.split(' ')
+    for index, word in enumerate(source_value):
+        first_upper_letter = source_value[index][0].upper()
+        source_value[index] = first_upper_letter + word[1:]
+    return ' '.join(source_value)
+
+
+def generator_of_greeting_message(name: str) -> str:
+    """
+    Возвращает текст приветствия с заглавными буквами в начале слов имени.
+
+    Args:
+        name: Имя пользователя
+
+    Returns:
+        str: Текст приветствия
+    """
+    formatted_name = set_first_letters_to_uppercase(name)
+    return 'Привет, {0}'.format(formatted_name)
--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,20 @@
 import pytest
-from main import *
+
+from main import generator_of_greeting_message
 
 
-@pytest.mark.parametrize( 'name,expected', [('Никита', 'Привет, Никита'), ('Ольга', 'Привет, Ольга')],)
+@pytest.mark.parametrize(
+    'name,expected', [
+        ('Никита', 'Привет, Никита'),
+        ('Ольга', 'Привет, Ольга'),
+    ],
+)
 def test_greeting(name: str, expected: str):
     """Текст приветствия зависит от имени."""
-    assert Greeting(name) == expected
+    assert generator_of_greeting_message(name) == expected
+
 
 def test_capitalize():
     """Все слова в имени начинаются с большой буквы."""
     name = 'яндекс практикум'
-    assert Greeting(name) == 'Привет, Яндекс Практикум'
+    assert generator_of_greeting_message(name) == 'Привет, Яндекс Практикум'
